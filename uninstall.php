dd31b618da6170fed2e129094cb0f57106a90eef
Jannes Magnusson <jannes.hp@t-online.de>
2023-01-19 17:15:53 +0100

    fix uninstall

--- uninstall.php
+++ uninstall.php
@@ -10,7 +10,9 @@ include_once dirname(__FILE__) . '/event.php';
 global $wpdb; // Must have this or else!
 
 $postmeta_table = $wpdb->postmeta;
+$posts_table = $wpdb->posts;
 
 foreach (AdventiEvent::fields as $field) {
 	$wpdb->query("DELETE FROM " . $postmeta_table . " WHERE meta_key = '". AD_EV_META . $field . "'");	
 }
+$wpdb->query("DELETE FROM " . $posts_table . " WHERE post_type = 'event'");
